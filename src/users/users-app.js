b0cbd6302e4e4c3cc63ccd530bdf42277a8045ac
Daniel Naranjo <naranjobenavidesd@gmail.com>
2025-04-02 08:11:06 +0200

    se crea visualmente el modal para poder crear el usuario

--- src/users/users-app.js
+++ src/users/users-app.js
@@ -2,6 +2,7 @@ import usersStore from './store/users-store';
 import { renderTable } from './presentation/render-table/render-table';
 import { renderButtons } from './presentation/render-buttons/render-buttons';
 import { renderAddButton } from './presentation/render-add-button/render-add-button';
+import { renderModal } from './presentation/render-modal/render-modal';
 
 /**
  * 
@@ -16,4 +17,5 @@ export const UsersApp = async (element) => {
     renderTable(element);
     renderButtons(element);
     renderAddButton(element);
+    renderModal(element);
 }
